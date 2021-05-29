8bad1140e4d2e4f1e29576d2b86e00b3626d9cdc
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-05-29 11:22:18 +0900

    修正

--- units/load.js
+++ units/load.js
@@ -1,3 +1,7 @@
 const fs = require("fs");
 const filename = process.argv[2];
-console.log(fs.readFileSync(filename, "utf-8") || "0");
+try {
+  console.log(fs.readFileSync(filename, "utf-8"));
+} catch (err) {
+  console.log("0");
+}
